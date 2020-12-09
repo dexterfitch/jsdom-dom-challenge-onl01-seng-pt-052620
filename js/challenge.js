ac21ab4fb6571d8faf5f1af2e0589dc0b3eea139
Dexter Fitch <fitch.dexter@gmail.com>
2020-12-09 05:05:00 +0000

    Automatically backed up by Learn

--- js/challenge.js
+++ js/challenge.js
@@ -3,6 +3,7 @@ const minus = document.getElementById("minus");
 const plus = document.getElementById("plus");
 const heart = document.getElementById("heart");
 const pause = document.getElementById("pause");
+const likes = document.querySelector("likes");
 
 let counterText = counter.textContent;
 let counterNum = parseInt(counterText);
